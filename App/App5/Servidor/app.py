5196290a8153d90cb67d63513bbd72a87edde680
Lufermor <luisfergoza@gmai.com>
2023-06-07 23:40:23 +0200

    Primeros pasos

---
+++ App/App5/Servidor/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+ma = Marshmallow(app)
+
+# Configuración de la base de datos                   username:password@hostname/database
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/tfg_dam_1"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# inicialización de la app con la extensión
+db = SQLAlchemy()
+db.init_app(app)
+
+class User(db.Model):
+    """Clase que representa a un usuario y lo vincula con la BBDD"""
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self,name, email):
+        """Crea una nueva instancia de la clase User con el name y email indicados"""
+        self.name = name
+        self.email = email
+
+class UserSchema(ma.Schema):
+    """Clase que define el formato de salida con marshmallow"""
+    class Meta:
+        """Atributos de la entidad User que se mostrarán"""
+        fields = ("id", "name", "email", "date")
+
+# Creamos instancias de la clase UserSchema
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+@app.route("/")
+def hello_world():
+    """Devuelve al usuario un hello world con la petición barra"""
+    return "<p>Hello, World!</p>"
+
+@app.route('/listusers', methods=['GET'])
+def listusers():
+    """Lista todos los usuarios de la base de datos y los devuelve 
+        formateados con ayuda de la clase UserSchema"""
+    all_users = User.query.all()
+    results = users_schema.dump(all_users)
+    return jsonify(results)
+
+@app.route('/useradd', methods=['POST'])
+def useradd():
+    """Añade un usuario a la BBDD con el name y email 
+        que llegan en la petición POST."""
+    name = request.json['name']
+    email = request.json['email']
+
+    user = User(name, email)
+    db.session.add(user)
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+
---
+++ App/Envia-Whatsapps/wspbot00001.py
@@ -0,0 +1,39 @@
+import pyautogui
+import webbrowser
+import pywhatkit
+from time import sleep
+
+webbrowser.open("https://web.whatsapp.com/send?phone=34680683625")  
+sleep(10)
+
+# pyautogui.typewrite("Hola como estas?\nEsta es la segunda línea0\nEsta es la tercera línea0")
+# pyautogui.press("enter")
+
+pywhatkit.sendwhatmsg("+34680683625", "Mensaje programado", 19, 23)
+# pywhatkit.sendwhatmsg_instantly("+34680683625", "Hola como estas?\nEsta es la segunda línea3\nEsta es la tercera línea3")
+# pyautogui.press("send")
+
+"""with open ("mensaje1.txt", "r") as file:
+    for line in file:
+        # pywhatkit.sendwhatmsg_instantly("+34680683625", line)
+        pyautogui.typewrite(line)
+        pyautogui.press("enter")"""
+
+"""with open ("mensaje1.txt", "r") as file:
+    for line in file:
+        pywhatkit.sendwhatmsg_instantly("+34680683625", line)
+        # pyautogui.typewrite(line)
+        # pyautogui.press("enter")"""
+
+'''with open ("mensaje1.txt", "r") as file:
+    for line in file:
+        # pywhatkit.sendwhatmsg_instantly("+34680683625", line)
+        pyautogui.typewrite(line)
+        """Nota: aunque no estamos presionando "enter" en cada iteración, 
+            cuando escribe una nueva línea, empuja a la anterior haciéndole imprimirse"""
+    pyautogui.press("enter")'''
+
+# paquete para llamadas: twilio:
+"""tienes que registrarte, obtener un id de cliente y una ¿key? 
+    Luego se tiene que crear un cliente que será el que haga las llamadas.
+    Es de pago pero tenemos 14 dólares de prueba."""
